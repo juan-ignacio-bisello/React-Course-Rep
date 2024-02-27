165c4f229788ac0eb5602f5cb9e4925da7db7f24
juanib99 <juanignaciobisello@hotmail.com>
2024-02-26 22:43:54 -0300

    +

---
+++ 01-counter-app/src/FirstApp.jsx
@@ -0,0 +1,12 @@
+export const FirstApp = () => {
+
+    const newMessage = 'Ignacio';
+    return ( 
+        // <> funciona como un Fracmento sin la necesidad de hacer la importación
+        <>
+            <h1>{ newMessage }</h1>
+            <p>No se </p>
+        </>
+        
+    );
+}
