a30a3dd4cecac27846c846ed5fcba52dce084b57
juanib99 <juanignaciobisello@hotmail.com>
2024-04-23 19:27:08 -0300

    HookApp

--- 05-hookApp/src/main.jsx
+++ 05-hookApp/src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { CounterApp } from './01-useState/CounterApp';
+import { CounterWithCustomHooks } from './01-useState/CounterWithCustomHooks';
 // import { HookApp } from './HooksApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterApp />
+    <CounterWithCustomHooks />
   </React.StrictMode>,
 )
