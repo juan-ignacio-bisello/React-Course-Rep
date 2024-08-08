4261698c4214fd688fb50a1d9441332449e693db
juanib99 <juanignaciobisello@hotmail.com>
2024-08-07 21:03:51 -0300

    fin: profundizando hooks-generales

--- 05-hookApp/src/main.jsx
+++ 05-hookApp/src/main.jsx
@@ -8,10 +8,14 @@ import { FormWithCustomHook } from './02-useEfect/FormWithCustomHook';
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 import { FocusScreen } from './04.useRef/FocusScreen';
 import { Layout } from './05-useLayaoutEffect/Layout';
+import { Memorize } from './06-memos/Memorize';
+import { MemoHook } from './06-memos/MemoHook';
+import { CallbackHook } from './06-memos/CallbackHook';
+import { Padre } from './07-tarea-memo/Padre';
 // import { HookApp } from './HooksApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Layout />
+    <Padre />
   </React.StrictMode>,
 )
