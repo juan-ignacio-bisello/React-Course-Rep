826faa5bb70fd82b0a1013c7459732503dabac5b
juanib99 <juanignaciobisello@hotmail.com>
2024-08-25 18:48:28 -0300

    useContex

---
+++ 05-hookApp/src/09-useContext/LogingPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const LogingPage = () => {
+  return (
+    <>
+        <h1>LogingPage</h1>
+        <hr />
+    </>
+  )
+}
--- 05-hookApp/src/main.jsx
+++ 05-hookApp/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import { CounterApp } from './01-useState/CounterApp';
 import { CounterWithCustomHooks } from './01-useState/CounterWithCustomHooks';
@@ -14,9 +15,13 @@ import { CallbackHook } from './06-memos/CallbackHook';
 import { Padre } from './07-tarea-memo/Padre';
 import './08-useReducer/intro-reducer';
 import { TodoApp } from './08-useReducer/TodoApp';
+import { MainApp } from './09-useContext/MainApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-    <TodoApp />
-//   </React.StrictMode>,
+    <BrowserRouter>
+        {/* <React.StrictMode> */}
+            <MainApp />
+        {/* </React.StrictMode>, */}
+    </BrowserRouter>
+   
 )
