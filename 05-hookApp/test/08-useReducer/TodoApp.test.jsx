8f68d9e33037bec171c259280b4cbe08ec6dd03d
juanib99 <juanignaciobisello@hotmail.com>
2025-01-15 23:56:51 -0300

    Pruebas en TodoApp

---
+++ 05-hookApp/test/08-useReducer/TodoApp.test.jsx
@@ -0,0 +1,39 @@
+import { render, screen } from "@testing-library/react"
+import { TodoApp } from "../../src/08-useReducer/TodoApp";
+import { useTodo } from "../../src/hooks/useTodo";
+
+
+jest.mock('../../src/hooks/useTodo')
+
+
+describe('Pruebas TodoApp', () => { 
+
+    useTodo.mockReturnValue({
+        todos: [
+            {
+                id: 1,
+                description: 'Todo 1',
+                done: false
+            },
+            {
+                id: 2,
+                description: 'Todo 2',
+                done: true
+            }
+        ],
+        handleDeleteTodo: jest.fn(),
+        handleToggleTodo: jest.fn(),
+        handleNewTodo: jest.fn(),
+        todosCount: 2,
+        pendingTodosCount: 1,
+    });
+
+    test('should mostrar el componente correctamente', () => { 
+        
+        render( <TodoApp /> );
+
+        expect( screen.getByText('Todo 1') ).toBeTruthy();
+        expect( screen.getByText('Todo 2') ).toBeTruthy();
+     });
+
+ });
