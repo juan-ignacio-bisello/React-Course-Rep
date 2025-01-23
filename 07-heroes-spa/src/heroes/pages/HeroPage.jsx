357c6ec4c0556d0248c615d8a7ea610d2b22cfe3
juanib99 <juanignaciobisello@hotmail.com>
2025-01-23 17:22:07 -0300

    animate.css

--- 07-heroes-spa/src/heroes/pages/HeroPage.jsx
+++ 07-heroes-spa/src/heroes/pages/HeroPage.jsx
@@ -19,12 +19,12 @@ export const HeroPage = () => {
   }
 
   return (
-     <div className='row mt-5'>
+     <div className='row mt-5 animate__animated animate__fadeInLeft'>
       <div className="col-4">
         <img 
           src={ `../assets/heroes/${ id }.jpg` }
           alt={ hero.superhero }
-          className="img-thumbnail animate__animated animate__fadeInLeft"
+          className="img-thumbnail"
         />
       </div>
 
