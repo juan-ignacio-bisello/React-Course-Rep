f75d1172a5c75bc98e00fc53046d50eb650a6157
juanib99 <juanignaciobisello@hotmail.com>
2025-02-06 07:12:30 -0300

    pruebas de types

---
+++ 07-heroes-spa/test/auth/types/types.test.js
@@ -0,0 +1,12 @@
+import { types } from "../../../src/auth/types/types";
+
+describe('Pruebas en types', () => { 
+
+    test('deben de regresar estos types', () => {
+
+        expect(types).toEqual({
+            login: '[Auth] Login',
+            logout: '[Auth] Logout'
+        });
+    });
+ })
