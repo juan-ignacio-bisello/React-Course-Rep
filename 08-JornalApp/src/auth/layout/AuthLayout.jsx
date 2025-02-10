479b0d4237f12f7343272e986e4049031158daef
juanib99 <juanignaciobisello@hotmail.com>
2025-02-10 20:07:03 -0300

    AuthLayout

---
+++ 08-JornalApp/src/auth/layout/AuthLayout.jsx
@@ -0,0 +1,25 @@
+import { Grid, Typography } from "@mui/material"
+
+export const AuthLayout = ({ children, title = '' }) => {
+  return (
+    <Grid container
+              spacing={0}
+              direction="column"
+              alignItems="center"
+              justifyContent="center"
+              sx={{ height: '100vh', backgroundColor: 'primary.main', padding: 4 }}
+             >
+            
+        <Grid item
+            xs={3}sm={6}md={4}
+            sx={{ width: {md: 45 }, backgroundColor:'white' , padding:3,borderRadius:2}}
+               >
+              
+          <Typography variant='h5' sx={{ mb: 1 }}>{ title }</Typography>
+
+          { children }
+
+        </Grid>
+    </Grid>
+  )
+}
--- 08-JornalApp/src/auth/pages/LoginPage.jsx
+++ 08-JornalApp/src/auth/pages/LoginPage.jsx
@@ -1,74 +1,60 @@
 import { Google } from '@mui/icons-material';
 import { Link as RouterLink} from 'react-router-dom';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { AuthLayout } from '../layout/AuthLayout';
  
 export const LoginPage = () => {
   return (
-    <Grid container
-    spacing={0}
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-            sx={{ height: '100vh', backgroundColor: 'primary.main', padding: 4 }}
-           >
-      
-      <Grid item
-          xs={3}sm={6}md={4}
-          sx={{backgroundColor:'white' , padding:3,borderRadius:2}}
-             >
-        
-        <Typography variant='h5' sx={{ mb: 1 }}>Login</Typography>
- 
- 
-          <Grid container spacing={2}>
-            <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField 
-                label="Correo" 
-                type="email" 
-                placeholder='correo@google.com' 
-                fullWidth
-              />
-            </Grid>
- 
-       <Grid item xs={ 12 } sx={{ mt: 2 }}>
-      <TextField 
-        label="Contraseña" 
-        type="password" 
-        placeholder='Contraseña' 
-        fullWidth
-      />
-      <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
-        
-        <Grid item xs={12} sm={6} >
-          <Button 
-            variant='contained' 
-            fullWidth
-          >
-            Login
-          </Button>
+    <AuthLayout title='Login'>
+      <form>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sx={{ mt: 2 }}>
+              <TextField 
+                  label="Correo" 
+                  type="email" 
+                  placeholder='correo@google.com' 
+                  fullWidth
+                />
+              </Grid>
+    
+         <Grid item xs={ 12 } sx={{ mt: 2 }}>
+        <TextField 
+          label="Contraseña" 
+          type="password" 
+          placeholder='Contraseña' 
+          fullWidth
+        />
+        <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+
+          <Grid item xs={12} sm={6} >
+            <Button 
+              variant='contained' 
+              fullWidth
+            >
+              Login
+            </Button>
+          </Grid>
+
+          <Grid item xs={12} sm={6} >
+            <Button variant='container' fullWidth >
+              <Google />
+              <Typography sx={{ ml: 1}}>Google</Typography>
+            </Button>
+          </Grid>
         </Grid>
 
-        <Grid item xs={12} sm={6} >
-          <Button variant='container' fullWidth >
-            <Google />
-            <Typography sx={{ ml: 1}}>Google</Typography>
-          </Button>
+        <Grid container direction='row' justifyContent='end'>
+          <Link component={ RouterLink } color='inherit' to="/auth/register">
+            Crear una cuenta
+          </Link>
         </Grid>
-      </Grid>
 
-      <Grid container direction='row' justifyContent='end'>
-        <Link component={ RouterLink } color='inherit' to="/auth/register">
-          Crear una cuenta
-        </Link>
-      </Grid>
+        </Grid>
 
-    </Grid>
-      
-            
-  </Grid>
- 
-  </Grid>
- 
-  </Grid>
+
+        </Grid>
+      </form>
+    </AuthLayout>
+    
   )
 }
