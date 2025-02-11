9522139570ed76d69f93ca2e66fa312ffd0e5e63
juanib99 <juanignaciobisello@hotmail.com>
2025-02-11 17:41:53 -0300

    Boton flotante

--- 08-JornalApp/src/journal/pages/JournalPages.jsx
+++ 08-JornalApp/src/journal/pages/JournalPages.jsx
@@ -1,6 +1,7 @@
-import { Typography } from '@mui/material'
+import { IconButton, Typography } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
 import { NoteView, NothingSelectedView } from '../views'
+import { AddOutlined } from '@mui/icons-material'
 
 export const JournalPages = () => {
   return (
@@ -9,8 +10,24 @@ export const JournalPages = () => {
         sdsgdhasddf
       </Typography> */}
 
-      <NoteView />
+      <NothingSelectedView />
+      {/* <NoteView /> */}
+
+      <IconButton 
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
       
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
+
     </JournalLayout>
   )
 }
