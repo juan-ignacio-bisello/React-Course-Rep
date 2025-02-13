23186b384c5e1ca77ce9cbb34c1d3e65137c6a32
juanib99 <juanignaciobisello@hotmail.com>
2025-02-13 16:30:50 -0300

    decrement y incrementby2

--- 09-toolkit-Redux/src/App.jsx
+++ 09-toolkit-Redux/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
-import { increment } from './store/slices/counter';
+import { decrement, increment, incrementBy2 } from './store/slices/counter';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -22,10 +22,18 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>count is { counter }</h1>
       <div className="card">
         <button onClick={ () => dispatch( increment() ) }>
-          count is { counter }
+          Increment
+        </button>
+
+        <button onClick={ () => dispatch( decrement() ) }>
+          Decrement
+        </button>
+
+        <button onClick={ () => dispatch( incrementBy2( 2 ) ) }>
+          Increment by 2
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
