20163979418b14fdf4c34c69ccbbd06bcfaf86d7
juanib99 <juanignaciobisello@hotmail.com>
2025-03-04 17:19:25 -0300

    Axios

--- 09-toolkit-Redux/src/store/slices/pokemon/thunks.js
+++ 09-toolkit-Redux/src/store/slices/pokemon/thunks.js
@@ -1,15 +1,20 @@
+import { pokemonApi } from '../../../api/pokemonApi';
 import { setPokemons, startLoadingPokemons } from './pokemonSlice';
 
 
 export const getPokemons = ( page = 0 ) => {
     
-    return ( dispach, getState ) => {
-        
+    return async( dispach, getState ) => {
+
         dispach( startLoadingPokemons() );
     
         // TODO: realizar peticion http
+        // const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${ page * 10 }`);
+        // const data = await resp.json();
 
-        // dispach( setPokemons() );
+        const { data } = await pokemonApi.get(`/pokemon?limit=10&offset=${ page * 10 }`)
+
+        dispach( setPokemons({ pokemon: data.results, page: page + 1 }) );
     }
 
 }
