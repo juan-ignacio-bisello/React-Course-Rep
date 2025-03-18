33f2ea0330336d1294885a3671db6182bb7777d4
juanib99 <juanignaciobisello@hotmail.com>
2025-03-18 15:41:17 -0300

    cloudinary SDK delete image + test

--- 08-JornalApp/test/helpers/fileUpload.test.js
+++ 08-JornalApp/test/helpers/fileUpload.test.js
@@ -1,4 +1,12 @@
-import { fileUpload } from "../../src/helpers/fileUpload"
+import { fileUpload } from "../../src/helpers/fileUpload";
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+    cloud_name: 'dwusbi8vo',
+    api_key: '834246187922523',
+    api_secret: 'fdqbuRTlDvj0Ue69hxT-5dRgMMU',
+    secure: true
+});
 
 describe('Pruebas en fileUpload', () => { 
 
@@ -13,5 +21,18 @@ describe('Pruebas en fileUpload', () => {
 
         expect( typeof url ).toBe('string');
 
-     })
+        const segments = url.split('/');
+        const imageId = segments[ segments.length - 1 ].replace( '.jpg', '' );
+
+        await cloudinary.api.delete_resources([ imageId ]);
+     });
+
+     test('should retornar null', async() => { 
+        
+        const file = new File( [], 'foto.jpg' );
+
+        const url = await fileUpload( file );
+
+        expect( url ).toBe( null );
+      })
  })
