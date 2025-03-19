cd83edc5ee0e2a16e8d013208c4ad5e87c6fdaff
juanib99 <juanignaciobisello@hotmail.com>
2025-03-19 13:29:05 -0300

    prueba completa sobre insercion

--- 08-JornalApp/test/store/journal/thunks.test.js
+++ 08-JornalApp/test/store/journal/thunks.test.js
@@ -1,7 +1,10 @@
 /**
  * @jest-environment node
  */
+import { collection, deleteDoc, getDocs } from "firebase/firestore/lite";
+import { addNewEmptyNote, savingNewNote, setActiveNote } from "../../../src/store/journal/journalSlice";
 import { startNewNote } from "../../../src/store/journal/thunks";
+import { FirebaseBD } from "../../../src/firebase/config";
 
 
 describe('Pruebas en Journal Thunks', () => {
@@ -20,5 +23,28 @@ describe('Pruebas en Journal Thunks', () => {
         
         await startNewNote()( dispatch, getState );
 
+        expect( dispatch ).toHaveBeenCalledWith( savingNewNote() );
+        expect( dispatch ).toHaveBeenCalledWith( addNewEmptyNote({
+            body: '',
+            title: '',
+            id: expect.any( String ),
+            date: expect.any( Number )
+        }));
+        expect( dispatch ).toHaveBeenCalledWith( setActiveNote({
+            body: '',
+            title: '',
+            id: expect.any( String ),
+            date: expect.any( Number )
+        }));
+
+        //Borrar de Firebase
+
+        const collectionRef = collection( FirebaseBD, `${ uid }/journal/note`);
+        const docs = await getDocs( collectionRef );
+
+        const deletePromises = [];
+        docs.forEach( doc => deletePromises.push( deleteDoc( doc.ref ) ) );
+
+        await Promise.all( deletePromises );
     });
 })
