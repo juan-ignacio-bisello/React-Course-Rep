da2cbf85e8f467980752363bdf0f8ccf09cdbb11
juanib99 <juanignaciobisello@hotmail.com>
2025-03-19 11:08:49 -0300

    Pruebas checkingCredential true y false

--- 08-JornalApp/test/store/auth/thunks.test.js
+++ 08-JornalApp/test/store/auth/thunks.test.js
@@ -1,5 +1,7 @@
-import { checkingCredential } from '../../../src/store/auth/authSlice';
-import { checkingAuthentication } from '../../../src/store/auth/thunks';
+import { singWithGoogle } from '../../../src/firebase/providers';
+import { checkingCredential, login, logout } from '../../../src/store/auth/authSlice';
+import { checkingAuthentication, startGoogleSignIn } from '../../../src/store/auth/thunks';
+import { demoUser } from '../../fixture/authFixture';
 
 jest.mock( '../../../src/firebase/providers' );
 
@@ -14,5 +16,27 @@ describe('Pruebas en thunks', () => {
         await checkingAuthentication()( dispatch );
         expect( dispatch ).toHaveBeenCalledWith( checkingCredential() );
          
-     })
+    });
+
+    test('startGoogleSignIn debe llamar checkingCredential y login - exito', async() => { 
+        
+        const loginData = { ok: true, ...demoUser };
+        await singWithGoogle.mockResolvedValue( loginData );
+
+        await startGoogleSignIn()( dispatch );
+
+        expect( dispatch ).toHaveBeenCalledWith( checkingCredential() );
+        expect( dispatch ).toHaveBeenCalledWith( login( loginData ) );
+    });
+    
+    test('startGoogleSignIn debe llamar checkingCredential y login - Error', async() => { 
+        
+        const loginData = { ok: false, errorMessage: 'Un error en Goole' };
+        await singWithGoogle.mockResolvedValue( loginData );
+
+        await startGoogleSignIn()( dispatch );
+
+        expect( dispatch ).toHaveBeenCalledWith( checkingCredential() );
+        expect( dispatch ).toHaveBeenCalledWith( logout( loginData.errorMessage ) );
+    });
  })
