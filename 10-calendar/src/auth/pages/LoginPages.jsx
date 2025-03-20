2f3703ce7d4acf686378546fde1faa9c2f206913
juanib99 <juanignaciobisello@hotmail.com>
2025-03-20 16:18:03 -0300

    rutas de la aplicacion

---
+++ 10-calendar/src/auth/pages/LoginPages.jsx
@@ -0,0 +1,7 @@
+
+
+export const LoginPages = () => {
+  return (
+    <div>LoginPages</div>
+  )
+}
---
+++ 10-calendar/src/router/AppRouter.jsx
@@ -0,0 +1,20 @@
+
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { LoginPages } from '../auth';
+import { CalendarPage } from '../calendar';
+
+export const AppRouter = () => {
+
+    const authStatus = 'not-authenticated';
+
+  return (
+    <Routes>
+        {
+            ( authStatus === 'not-authenticated' )  //not-authenticated     authenticated
+            ? <Route path="/auth/*" element={ <LoginPages /> } />
+            : <Route path="/*" element={ <CalendarPage /> } />
+        }
+        <Route path="/*" element={ <Navigate to="/auth/login" /> } />
+    </Routes>
+  )
+}
