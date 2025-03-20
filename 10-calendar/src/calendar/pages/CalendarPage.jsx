cf3dae833ec2239321f6604feb8d309955924d18
juanib99 <juanignaciobisello@hotmail.com>
2025-03-20 17:27:43 -0300

    React big calendar

--- 10-calendar/src/calendar/pages/CalendarPage.jsx
+++ 10-calendar/src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,46 @@
 import { Navbar } from '../components/Navbar';
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { addHours, getDay, format, parse, startOfWeek } from 'date-fns';
+import enUS from 'date-fns/locale/en-US';
 
+const locales = {
+  'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [{
+  title: 'Cumpleaños',
+  notes: 'Comprar carne',
+  start: new Date(),
+  end: addHours( new Date(), 2 ),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Ignacio'
+  }
+}]
 
 export const CalendarPage = () => {
+
   return (
     <>
       <Navbar />
+
+      <Calendar
+        localizer={localizer}
+        events={ events }
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc( 100vh - 80px )' }}
+      />
     </>
   )
 }
