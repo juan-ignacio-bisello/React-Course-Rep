93ca984144c1c35fa6c2544ff0f400bee4d84503
juanib99 <juanignaciobisello@hotmail.com>
2025-03-21 17:48:30 -0300

    info del formulario del evento

--- 10-calendar/src/calendar/components/CalendarModal.jsx
+++ 10-calendar/src/calendar/components/CalendarModal.jsx
@@ -1,4 +1,4 @@
-import { addHours } from 'date-fns';
+import { addHours, differenceInSeconds } from 'date-fns';
 import { useState } from 'react';
 import Modal from 'react-modal';
 import DatePicker, {registerLocale } from 'react-datepicker';
@@ -51,6 +51,19 @@ export const CalendarModal = () => {
         setIsOpen( false );
     }
 
+    const onSubmit = ( event ) => {
+        event.preventDeafult();
+        const difference = differenceInSeconds( formValues.end, formValues.start );
+        if ( isNaN( difference ) || difference <= 0 ) {
+            console.log('ERROR en las fechas');
+            return;
+        }
+
+        if ( formValues.title.length <= 0 ) return;
+
+        console.log(formValues);
+    }
+
   return (
     <Modal
         isOpen={ isOpen }
@@ -62,7 +75,7 @@ export const CalendarModal = () => {
     >
         <h1> Nuevo evento </h1>
     <hr />
-    <form className="container">
+    <form className="container" onSubmit={ onSubmit }>
 
         <div className="form-group mb-2">
             <label>Fecha y hora inicio</label>
