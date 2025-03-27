06d722939522fc4673f5f77ed2f0c30bfeb595bb
juanib99 <juanignaciobisello@hotmail.com>
2025-03-26 23:59:29 -0300

    Configurando Express

--- 11-calendar-backend/index.js
+++ 11-calendar-backend/index.js
@@ -1,1 +1,17 @@
-console.log('hola mundo!');
+const express = require('express');
+
+//crear servidor de express
+const app = express();
+
+// rutas
+app.get('/', ( req, res ) => {
+    
+    res.json({
+        "ok": true
+    })
+})
+
+// escuchar peticion
+app.listen( 6000, () => {
+    console.log(`servidor en puerto ${ 6000 }` );
+} )
