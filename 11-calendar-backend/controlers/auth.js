2f6e0a5a258b8f8997543e730ad80b94742faa53
juanib99 <juanignaciobisello@hotmail.com>
2025-03-29 14:40:14 -0300

    Express Validator

--- 11-calendar-backend/controlers/auth.js
+++ 11-calendar-backend/controlers/auth.js
@@ -1,18 +1,20 @@
 const { response } = require('express');
-
+const { validationResult } = require('express-validator');
 
 const crearUsuario = ( req, res = response ) => {
 
     const { name, email, password } = req.body;
 
-    if( name.length < 5 ) {
-        return res.status(400).json({
-            "ok": false,
-            msg: 'nombre de + 5 letras',
-        })
+    // Manejo de errores
+    const errors = validationResult( req );
+    if ( !errors.isEmpty() ) {
+        return res.status( 400 ).json({
+            ok: false,
+            errors: errors.mapped()
+        });
     }
 
-    res.json({
+    res.status(201).json({
         "ok": true,
         msg: 'registro',
         name,
@@ -24,8 +26,17 @@ const crearUsuario = ( req, res = response ) => {
 const loginUsuario = ( req, res = response  ) => {
     
     const { email, password } = req.body;
+    
+    // Manejo de errores
+    const errors = validationResult( req );
+    if ( !errors.isEmpty() ) {
+        return res.status( 400 ).json({
+            ok: false,
+            errors: errors.mapped()
+        });
+    }
 
-    res.json({
+    res.status(200).json({
         "ok": true,
         msg: 'login',
         email,
--- 11-calendar-backend/routes/auth.js
+++ 11-calendar-backend/routes/auth.js
@@ -4,7 +4,9 @@
 */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
 const router = Router();
+
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controlers/auth')
 
 // router.get('/', ( req, res ) => {
@@ -15,11 +17,25 @@ const { crearUsuario, loginUsuario, revalidarToken } = require('../controlers/au
 // });
 
 
-router.get('/renew', revalidarToken );
+router.get(
+    '/renew', revalidarToken );
 
-router.post('/new', crearUsuario );
+router.post(
+    '/new', 
+    [   // middlewares
+        check('name', 'El nombre es obligatorio' ).not().isEmpty(),
+        check('email', 'El email es obligatorio' ).isEmail(),
+        check('password', 'El password debe ser de 6 caracteres' ).isLength({ min: 6 })
+    ],
+     crearUsuario );
 
-router.post('/', loginUsuario );
+router.post(
+    '/', 
+    [   // middlewares
+        check('email', 'El email es obligatorio' ).isEmail(),
+        check('password', 'El password debe ser de 6 caracteres' ).isLength({ min: 6 })
+    ],
+     loginUsuario );
 
 
 
