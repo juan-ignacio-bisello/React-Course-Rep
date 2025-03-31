b42c7267a757eb1fd571aabe8cdff91411d3bb12
juanib99 <juanignaciobisello@hotmail.com>
2025-03-30 23:25:00 -0300

    Generar un Json Web Token

--- 11-calendar-backend/controllers/auth.js
+++ 11-calendar-backend/controllers/auth.js
@@ -1,6 +1,7 @@
 const { response } = require('express');
 const bcrypt = require('bcryptjs');
 const Usuario = require('../models/Usuario');
+const { generarJWT } = require('../helpers/jwt');
 
 const crearUsuario = async( req, res = response ) => {
 
@@ -25,10 +26,14 @@ const crearUsuario = async( req, res = response ) => {
 
         await usuario.save();
 
+        // Generar JWT
+        const token = await generarJWT( usuario.id, usuario.name );
+
         res.status(201).json({
             "ok": true,
             uid: usuario.id,
-            name: usuario.name
+            name: usuario.name,
+            token
         });
 
     } catch (error) {
@@ -67,12 +72,15 @@ const loginUsuario = async( req, res = response  ) => {
             });
         }
 
-        // Generar JWT
 
-        res.json({
+        // Generar JWT
+        const token = await generarJWT( usuario.id, usuario.name );
+
+        res.status(201).json({
             ok: true,
             uid: usuario.id,
-            name: usuario.name
+            name: usuario.name,
+            token
         })
 
     } catch (error) {
