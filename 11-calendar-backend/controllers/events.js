f487f5dcdbfc5f08fdb5e6d209c3d085a54cba52
juanib99 <juanignaciobisello@hotmail.com>
2025-04-04 00:24:14 -0300

    Grabar el evento en la base de datos

--- 11-calendar-backend/controllers/events.js
+++ 11-calendar-backend/controllers/events.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+const Evento = require("../models/Evento");
 
 
 const getEvent = ( req, res = response  ) => {
@@ -9,10 +10,28 @@ const getEvent = ( req, res = response  ) => {
     })
 }
 
-const crearEvento = ( req, res = response  ) => {
+const crearEvento = async( req, res = response  ) => {
 
-    console.log(req.body);
+    const evento = new Evento( req.body );
 
+    try {
+
+        evento.user = req.uid;
+        
+        const eventoGuardado = await evento.save()
+
+        res.json({
+            ok: true,
+            evento: eventoGuardado
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Habla con el ADMIN'
+        });
+    }
 
     res.json({
         "ok": true,
