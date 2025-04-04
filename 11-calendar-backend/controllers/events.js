73e612e408c7a2aa598ae47e98a962c7923dcb8a
juanib99 <juanignaciobisello@hotmail.com>
2025-04-04 01:33:01 -0300

    Actualizar un Evento

--- 11-calendar-backend/controllers/events.js
+++ 11-calendar-backend/controllers/events.js
@@ -39,12 +39,48 @@ const crearEvento = async ( req, res = response ) => {
 }
 
 
-const actualizarEvento = ( req, res = response  ) => {
+const actualizarEvento = async( req, res = response  ) => {
 
-    res.json({
-        "ok": true,
-        msg: 'actualizarEventos'
-    })
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        
+        const evento = await Evento.findById( eventoId );
+
+        if ( !evento ) {
+            res.status(404).json({
+                ok: false,
+                msg: 'no existe un evento con ese id'
+            });;
+        }
+
+        if ( evento.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'no tiene permisos para editar'
+            });
+        }
+
+        const nuevoEvento = {
+            ...req.body,
+            user: uid
+        }
+
+        const eventoActualizado = await Evento.findByIdAndUpdate( eventoId, nuevoEvento, { new: true } );
+
+        res.json({
+            ok: true,
+            evento: eventoActualizado
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+    }
 }
 
 const eliminarEvento = ( req, res = response  ) => {
