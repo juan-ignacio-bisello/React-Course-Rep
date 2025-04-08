339bc797fece5aef087ccb6f2e652b0968201617
juanib99 <juanignaciobisello@hotmail.com>
2025-04-08 00:17:42 -0300

    AuthSlice

--- 10-calendar-frontend/src/store/store.js
+++ 10-calendar-frontend/src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { calendarSlice, uiSlice } from './';
+import { authSlice } from '../store/auth/authSlice';
 
 export const store = configureStore({
     reducer: {
+        auth: authSlice.reducer,
         calendar: calendarSlice.reducer,
         ui: uiSlice.reducer
     },
