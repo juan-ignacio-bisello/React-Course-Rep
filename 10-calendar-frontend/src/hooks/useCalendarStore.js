a556b46465e1f208a229c15211e9cc6db94cb5d5
juanib99 <juanignaciobisello@hotmail.com>
2025-04-09 14:41:09 -0300

    Cargar los eventos al store

--- 10-calendar-frontend/src/hooks/useCalendarStore.js
+++ 10-calendar-frontend/src/hooks/useCalendarStore.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { onAddNewEvent, onDeleteEvent, onSetActiveEvent, onUpdateEvent } from "../store/calendar/calendarSlice";
+import { onAddNewEvent, onDeleteEvent, onLoadEvents, onSetActiveEvent, onUpdateEvent } from "../store/calendar/calendarSlice";
 import { calendarApi } from '../api'
 import { convertToDateEvents } from "../helper";
 
@@ -33,7 +33,8 @@ export const useCalendarStore = () => {
     try {
       
       const { data } = await calendarApi.get('/events');
-      const events = convertToDateEvents( data.eventos);
+      const events = convertToDateEvents( data.eventos );
+      dispatch( onLoadEvents( events ) );
       console.log( events );
 
     } catch (error) {
