e76c52be434079dd143e434cae08e3e19f23e168
juanib99 <juanignaciobisello@hotmail.com>
2025-04-11 16:49:15 -0300

    Actualizar evento

--- 10-calendar-frontend/src/hooks/useCalendarStore.js
+++ 10-calendar-frontend/src/hooks/useCalendarStore.js
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
+import Swal from 'sweetalert2';
 import { onAddNewEvent, onDeleteEvent, onLoadEvents, onSetActiveEvent, onUpdateEvent } from "../store/calendar/calendarSlice";
 import { calendarApi } from '../api'
 import { convertToDateEvents } from "../helper";
@@ -14,14 +15,25 @@ export const useCalendarStore = () => {
   }
 
   const startSavingEvent = async( calendarEvent ) => {
-    if( calendarEvent._id ) {
-      dispatch( onUpdateEvent({ ...calendarEvent }) );
-    } else {
-      
+
+    try {
+
+      if ( calendarEvent.id ) {
+
+        await calendarApi.put(`/events/${ calendarEvent.id }`, calendarEvent );
+        dispatch( onUpdateEvent({ ...calendarEvent, user }) );
+        return;
+  
+      } 
+       
       const { data } = await calendarApi.post( '/events', calendarEvent );
-      
       dispatch( onAddNewEvent({ ...calendarEvent, id: data.evento.id, user }) );
+
+    } catch (error) {
+      console.log(error);
+      Swal.fire( 'Error al guardar', error.response.data.msg, 'error' );
     }
+    
   }
 
   const startDeletingEvent = () => {
