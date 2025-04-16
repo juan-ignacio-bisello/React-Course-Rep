e19ab0a8630cd24eb1cc0158ac498f8c2b522124
juanib99 <juanignaciobisello@hotmail.com>
2025-04-15 23:19:52 -0300

    Probando estados y acciones del authSlice

--- 10-calendar-frontend/test/store/auth/authSlice.test.js
+++ 10-calendar-frontend/test/store/auth/authSlice.test.js
@@ -1,5 +1,6 @@
-const { authSlice } = require("../../../src/store/auth/authSlice");
-const { initialState } = require("../../fixtures/authStates");
+const { authSlice, onLogin, onLogout, clearErrorMessage, onChecking } = require("../../../src/store/auth/authSlice");
+const { initialState, authenticatedState } = require("../../fixtures/authStates");
+const { testUserCredentials } = require("../../fixtures/testUser");
 
 describe('Pruebas en authSlice', () => { 
     
@@ -8,4 +9,51 @@ describe('Pruebas en authSlice', () => {
         expect( authSlice.getInitialState() ).toEqual( initialState );
 
     });
+
+    test('should realizar un login', () => { 
+        
+        const state = authSlice.reducer( initialState, onLogin( testUserCredentials ) );
+        expect( state ).toEqual({
+            status: 'authenticated',
+            user: testUserCredentials,
+            errorMessage: undefined,
+        });
+    });
+
+    test('should realizar el logout', () => { 
+        
+        const state = authSlice.reducer( authenticatedState, onLogout() );
+        expect( state ).toEqual({
+            status: 'not-authenticated',
+            user: {},
+            errorMessage: undefined,
+        });
+    });
+
+    test('should realizar el logout', () => { 
+        const errorMessage = 'Credenciales no validas';
+        const state = authSlice.reducer( authenticatedState, onLogout( errorMessage ) );
+        expect( state ).toEqual({
+            status: 'not-authenticated',
+            user: {},
+            errorMessage: errorMessage,
+        });
+    });
+
+    test('should limpiar el errorMessage', () => { 
+        
+        const errorMessage = 'Credenciales no validas';
+        const state = authSlice.reducer( authenticatedState, onLogout( errorMessage ) );
+        const newState = authSlice.reducer( state, clearErrorMessage() );
+
+        expect( newState.errorMessage ).toBe( undefined );
+    });
+
+    test('should llamar el onChecking', () => { 
+        
+        const state = authSlice.reducer( initialState, onLogin( testUserCredentials ) );
+        const newState = authSlice.reducer( state, onChecking() );
+
+        expect( newState ).toEqual( initialState );
+     })
  })
