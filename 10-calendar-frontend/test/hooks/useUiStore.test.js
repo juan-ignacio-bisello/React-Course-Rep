180994bd1e51893d279bb1f8dfd8988120bd3df4
juanib99 <juanignaciobisello@hotmail.com>
2025-04-17 19:04:18 -0300

    Pruebas en useUiStore

--- 10-calendar-frontend/test/hooks/useUiStore.test.js
+++ 10-calendar-frontend/test/hooks/useUiStore.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react"
+import { act, renderHook } from "@testing-library/react"
 import { useUiStore } from "../../src/hooks"
 import { Provider } from "react-redux";
 import { store, uiSlice } from "../../src/store";
@@ -34,5 +34,58 @@ describe('Pruebas en useUiStore', () => {
             toggleDateModal: expect.any( Function ),  
         });
 
+    });
+
+    test('openDateModal debe colocar true en el isDateModalOpen', () => { 
+        
+        const mockStore = getMockStore({ isDateModalOpen: false });
+        const { result } = renderHook( () => useUiStore(), {
+            wrapper: ({ children }) => <Provider store={ mockStore }>{ children }</Provider>
+        
+        });
+
+        const { openDateModal } = result.current;
+
+        act( () => {
+            openDateModal();
+        });
+
+        expect(result.current.isDateModalOpen).toBeTruthy();
+    });
+
+    test('closeDateModal debe colocar false en isDateModalOpen', () => { 
+    
+        const mockStore = getMockStore({ isDateModalOpen: true });
+        const { result } = renderHook( () => useUiStore(), {
+            wrapper: ({ children }) => <Provider store={ mockStore }>{ children }</Provider>
+        
+        });
+
+        act( () => {
+            result.current.closeDateModal();
+        });
+
+        expect( result.current.isDateModalOpen ).toBeFalsy();
+    });
+
+    test('toggleDateModal debe cambiar el estado', () => { 
+    
+        const mockStore = getMockStore({ isDateModalOpen: true });
+        const { result } = renderHook( () => useUiStore(), {
+            wrapper: ({ children }) => <Provider store={ mockStore }>{ children }</Provider>
+        
+        });
+
+        act( () => {
+            result.current.toggleDateModal();
+        });
+
+        expect( result.current.isDateModalOpen ).toBeFalsy();
+
+        act( () => {
+            result.current.toggleDateModal();
+        });
+
+        expect( result.current.isDateModalOpen ).toBeTruthy();
     });
  })
